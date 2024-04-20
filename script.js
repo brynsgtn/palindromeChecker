6a8cfe6362267c7aa3d614746bab38fdebe9f136
brynsgtn <bryansuguitan17@gmail.com>
2024-04-20 12:52:54 -0500

    Finalized all files

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// Variable declarations
 const checkButtonEl = document.getElementById("check-btn");
 const inputValueEl = document.getElementById("text-input");
 const resultEl = document.getElementById("result");
 
-checkButtonEl.addEventListener("click", function() {
+// Check palindrome function
+const checkPalindrome = () => {
   let value = inputValueEl.value;
   let re = /[\W_]/g;
   let lowerCaseValue = value.toLowerCase().replace(re, "");
@@ -16,4 +18,14 @@ checkButtonEl.addEventListener("click", function() {
   } else {
     resultEl.innerHTML = `<p>"<em>${value}</em>" is a <strong>NOT</strong> palindrome</p>`
   }
-});
+}
+
+
+// Event listeners
+checkButtonEl.addEventListener("click", checkPalindrome);
+
+inputValueEl.addEventListener("keypress", function(event) {
+  if(event.key == "Enter") {
+    checkPalindrome();
+  }
+})
